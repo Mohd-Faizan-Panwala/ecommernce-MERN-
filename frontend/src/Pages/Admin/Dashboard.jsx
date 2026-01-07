70ce47031fc906e2c5cc64683b81059ac8b8e2e7
Mohd Faizan Panwala <faizanpanwala110@gmail.com>
2026-01-07 09:43:00 +0530

    Working on products module

--- frontend/src/Pages/Admin/Dashboard.jsx
+++ frontend/src/Pages/Admin/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
-export default function AdminDashboard() {
+export default function AdminDashboard() {//admin dashboard to view total orders 
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
