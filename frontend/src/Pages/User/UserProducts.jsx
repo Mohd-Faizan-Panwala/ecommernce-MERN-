5a641824dcbb8496c755c1f49b552491db5f7706
Mohd Faizan Panwala <faizanpanwala110@gmail.com>
2026-01-08 07:30:00 +0530

    Working on orders module

--- frontend/src/Pages/User/UserProducts.jsx
+++ frontend/src/Pages/User/UserProducts.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import {useNavigate}  from "react-router-dom";
 import toast from "react-hot-toast";
 
-export default function UserProducts() {
+export default function UserProducts() { //user can view products and place orders
    const navigate = useNavigate();
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
