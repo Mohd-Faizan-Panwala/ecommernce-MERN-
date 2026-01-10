9def0b4d9dc300b6aad27090bba21f61b4cbe2c6
Mohd Faizan Panwala <faizanpanwala110@gmail.com>
2026-01-10 10:30:00 +0530

    Working on orders module

--- backend/Routes/product.route.js
+++ backend/Routes/product.route.js
@@ -7,7 +7,7 @@ const upload = require("../Middleware/upload");
 
 const router = express.Router();
 
-router.get("/",getAllProducts);
+router.get("/",getAllProducts);//to render the orders in admin side 
 router.post("/",auth,adminOnly,upload.single("image"),addProduct); //add product
 router.put("/:id",auth,adminOnly,upload.single("image"),updateProduct); //update the existing product
 router.delete("/:id", auth, adminOnly, deleteProduct);//delet the existing product from the db 
