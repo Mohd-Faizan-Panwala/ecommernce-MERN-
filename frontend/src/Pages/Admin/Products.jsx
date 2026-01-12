fc080f7fa7fbcbf6b364991e13e7920e8ec7587f
Mohd Faizan Panwala <faizanpanwala110@gmail.com>
2026-01-12 09:37:12 +0530

    Working on Stock Management

--- frontend/src/Pages/Admin/Products.jsx
+++ frontend/src/Pages/Admin/Products.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import toast from "react-hot-toast";
 
-export default function AdminProducts() {//for admin side to add products so users can place orders
+export default function AdminProducts() {//for admin side to add products  users can place orders
   const navigate = useNavigate()
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
